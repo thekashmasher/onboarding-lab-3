836708f30eae1fc44764df7ad19724816675d4b2
thekashmasher <120293801+thekashmasher@users.noreply.github.com>
2025-12-25 00:22:05 -0500

    Update exercise4.cpp
    updated exericse3

--- dv/exercise4.cpp
+++ dv/exercise4.cpp
@@ -1,5 +1,33 @@
+#include <cstdint>
+#include <catch2/catch_test_macros.hpp>
 #include <VExercise4.h>
 
-int main() {
-  VExercise4 model;
+TEST_CASE("Exercise4: Decoder behavior") {
+    VExercise4 model;
+
+    model.alpha = 0xAA;
+    model.beta  = 0x55;
+    model.gamma = 0x0F;
+
+    // cs = 0, sel is don't care
+    model.cs = 0;
+    for (int sel = 0; sel < 4; ++sel) {
+        model.sel = sel;
+        model.eval();
+        REQUIRE(model.out == 0);
+    }
+
+    model.cs = 1;
+
+    model.sel = 0; model.eval();
+    REQUIRE(model.out == model.alpha);
+
+    model.sel = 1; model.eval();
+    REQUIRE(model.out == model.beta);
+
+    model.sel = 2; model.eval();
+    REQUIRE(model.out == model.gamma);
+
+    model.sel = 3; model.eval();
+    REQUIRE(model.out == (model.alpha & (model.beta | model.gamma)));
 }
